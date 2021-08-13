d9df57020b7b854efd5948ef028e114d074a35d7
shashidhar788 <rsreddy1378@gmail.com>
2021-08-13 01:44:10 -0400

    google api endpoint

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/dataRouter');
+var google = require('./routes/google')
 
 var app = express();
 app.use(cors())
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/data',dataRouter)
 app.use('/users', usersRouter);
+app.use('/google',google)
 
 /* // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/google.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const {Client} = require("@googlemaps/google-maps-services-js");
+
+
+
+require('dotenv').config();
+
+/* GET google query of lat and lon. */
+router.get('/', async function(req, res, next) {
+
+    const client = new Client({});
+    try{
+        
+        const response = await client.placeDetails({
+            params:{
+                place_id:req.body.placeId,
+                key: process.env.GOOGLE_API
+            }
+        })
+        
+        console.log("the response from api client is ", response);
+        res.send(JSON.stringify(response.data.result.geometry.location))
+
+    }catch(e){
+        
+        res.status(400).json({"error": e.message, "details":e});
+
+
+    }
+
+    
+        
+  
+});
+
+module.exports = router;
