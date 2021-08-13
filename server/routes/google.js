6c148b6455983b2c777695ac846103a46b36585b
shashidhar788 <rsreddy1378@gmail.com>
2021-08-13 02:29:29 -0400

    added tests for google endpoint

--- server/routes/google.js
+++ server/routes/google.js
@@ -9,21 +9,19 @@ require('dotenv').config();
 /* GET google query of lat and lon. */
 router.get('/', async function(req, res, next) {
 
-    if(req.body.placeId==null){
-        res.send("Hello from google endpoint")
-    }
+    
 
     const client = new Client({});
     try{
         
         const response = await client.placeDetails({
             params:{
-                place_id:req.body.placeId,
+                place_id:req.query.placeId,
                 key: process.env.GOOGLE_API
             }
         })
         
-        console.log("the response from api client is ", response);
+        console.log("the response from api client is ", response.data.result.geometry.location);
         res.send(JSON.stringify(response.data.result.geometry.location))
 
     }catch(e){
--- server/test/index.js
+++ server/test/index.js
@@ -1,10 +1,20 @@
 const request = require("supertest");
 const app = require("../app");
 const db = require('../db')
+const {Client} = require("@googlemaps/google-maps-services-js");
+var expect = require('chai')
 
+require('dotenv').config();
 describe("GET /", () => {
   it("responds with Hello Express", (done) => {
     request(app).get("/").expect("Hello Express", done);
   })
 });
 
+describe("GET /google", () => {
+  it("responds with a latitude and logitude",   (done) => {
+    
+    request(app).get('/google').query({placeId:"ChIJrTLr-GyuEmsRBfy61i59si0"})
+    .expect({"lat":-33.8676569,"lng":151.2017213},done())
+  })
+});
